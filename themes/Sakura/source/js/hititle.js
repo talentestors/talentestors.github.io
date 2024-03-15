adbc3587b3e8e6848d2cb38e44018cbec418b0ac
talentestors <talentestors@gmail.com>
2024-03-15 15:44:11 +0800

    backup my blog of https://gitee.com/talentestors/my-blog-hexo.git

--- themes/Sakura/source/js/hititle.js
+++ themes/Sakura/source/js/hititle.js
@@ -1,16 +1,1 @@
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = '(っ °Д °;)っ不要走！';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
-        document.title = 'ヾ(•ω•`)o欢迎回来！' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/trhx2.png"),document.title="(っ °Д °;)っ不要走！",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/img/trhx2.png"),document.title="ヾ(•ω•`)o欢迎回来！"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
